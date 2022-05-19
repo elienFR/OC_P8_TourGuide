065e663c5a77603852d4b17505bdbd1d276398a4
Elien <elienferre@yahoo.fr>
2022-05-19 19:47:41 +0200

    created microservices for gpsUtil and rewardCentral

--- rewardcentralms/src/main/java/com/elienferre/rewardscentralms/RewardCentralMsApplication.java
+++ rewardcentralms/src/main/java/com/elienferre/rewardscentralms/RewardCentralMsApplication.java
@@ -3,11 +3,14 @@ package com.elienferre.rewardscentralms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
-public class RewardscentralmsApplication {
+public class RewardCentralMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RewardscentralmsApplication.class, args);
+		Locale.setDefault(Locale.FRENCH);
+		SpringApplication.run(RewardCentralMsApplication.class, args);
 	}
 
 }
