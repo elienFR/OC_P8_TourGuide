fda63a48a64699604479b76d42d866dc67e5ea68
Elien <elienferre@yahoo.fr>
2022-05-20 09:36:57 +0200

    init trippricerms

---
+++ trippricerms/src/main/java/com/elienferre/trippricerms/TrippricermsApplication.java
@@ -0,0 +1,13 @@
+package com.elienferre.trippricerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippricermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippricermsApplication.class, args);
+	}
+
+}
